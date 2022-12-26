16206713422645227ab7931d19b9cc8c57bf9c28
aysbird <aysebirdal01@gmail.com>
2022-12-25 20:50:09 -0500

    Second Commit

---
+++ src/main/java/org/MyFirstProject/HomeWork/GitHomeWork2.java
@@ -0,0 +1,12 @@
+package org.MyFirstProject.HomeWork;
+
+public class GitHomeWork2 {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k =(j += i)/5;
+        System.out.println(i + " : " + j + " : " + k);
+
+        // A. 10:30:6
+    }
+}
